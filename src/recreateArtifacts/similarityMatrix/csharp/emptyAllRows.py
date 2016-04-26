5ecee19e81e7af2e3cd2e6f658d780e19825d0d3
Carl Chapman <carlallenchapman@gmail.com>
2016-04-26 07:51:49 -0500

    python row remover works now

--- src/recreateArtifacts/similarityMatrix/csharp/emptyAllRows.py
+++ src/recreateArtifacts/similarityMatrix/csharp/emptyAllRows.py
@@ -5,7 +5,13 @@ import sys, os
 # only run if you really want to empty all the row directory contents
 
 # cite: http://stackoverflow.com/questions/2212643/python-recursive-folder-read
-for root, subdirs, files in os.walk(os.path.join(sys.path[0],"allRows"):
+allRowsAbsPath = os.path.join(sys.path[0],"allRows")
+for root, subdirs, files in os.walk(allRowsAbsPath):
+    print("starting walk")
     for subdir in subdirs:
-        for filename in files:
-            os.remove(os.path.join(root, filename))
+        thisSubdirAbsPath = os.path.join(allRowsAbsPath,subdir)
+        print("in subdir: "+str(thisSubdirAbsPath))
+        for root, subdirs, files  in os.walk(thisSubdirAbsPath):
+            for filename in files:
+                print("removing file: "+str(filename))
+                os.remove(os.path.join(thisSubdirAbsPath, filename))
