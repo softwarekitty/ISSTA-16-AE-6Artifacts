37b0471209f99bb2855e1b3e8ac0c625330c859a
Carl Chapman <carlallenchapman@gmail.com>
2016-04-26 22:10:30 -0500

    proofs complete, we can generate a lookup just as good as the old one in memory, slaughing off an extranious artifact and going much faster

--- src/recreateArtifacts/MainScratch.java
+++ src/recreateArtifacts/MainScratch.java
@@ -1,16 +1,9 @@
 package recreateArtifacts;
 
-import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -26,19 +19,26 @@ import main.parse.QuoteRuleException;
 public class MainScratch {
 	public static void main(String[] args) throws IllegalArgumentException, IOException, QuoteRuleException,
 			PythonParsingException, ClassNotFoundException, SQLException {
-		
+
 		TreeSet<RegexProjectSet> corpus = LoadUtil
 				.loadRegexProjectSetInput(IOUtil.readLines(PathUtil.pathToCorpusFile()));
 
 		String wholeOrderingFileContents = IOUtil.readFileToString(PathUtil.pathToOriginalOrdering());
-		HashMap<String,Integer> pimFile = getPatternIndexMap(wholeOrderingFileContents);
+		HashMap<String, Integer> pimFile = getPatternIndexMap(wholeOrderingFileContents);
 		HashMap<String, Integer> pimMem = getPatternIndexMapFromCorpus(corpus);
+
+		// prints true
+		System.out.println("PIMs equal: " + pimFile.equals(pimMem));
+		
+		HashMap<Integer, RegexProjectSet> oldLookup = getLookupOld(corpus,pimFile);
+		HashMap<Integer, RegexProjectSet> newLookup = getLookupNew(corpus);
 		
 		// prints true
-		System.out.println("equal: "+pimFile.equals(pimMem));
+		System.out.println("Lookupss equal: " + oldLookup.equals(newLookup));
+
 	}
-	
-	public static HashMap<Integer, RegexProjectSet> getLookup(TreeSet<RegexProjectSet> corpus,
+
+	public static HashMap<Integer, RegexProjectSet> getLookupOld(TreeSet<RegexProjectSet> corpus,
 			HashMap<String, Integer> patternIndexMap) {
 		HashMap<Integer, RegexProjectSet> lookup = new HashMap<Integer, RegexProjectSet>();
 
@@ -50,16 +50,41 @@ public class MainScratch {
 		return lookup;
 	}
 
-	private static String getMapContent(TreeMap<String,Integer> anyMap) {
+	public static HashMap<Integer, RegexProjectSet> getLookupNew(TreeSet<RegexProjectSet> corpus) {
+		/**
+		 * define a comparator that orders the regexes by rawPattern, as this
+		 * was the order used before exporting to Rex, and is needed to map the
+		 * indices from clusters back to patterns
+		 * 
+		 * @author cc
+		 *
+		 */
+		class PatternComparator implements Comparator<RegexProjectSet> {
+			@Override
+			public int compare(RegexProjectSet r1, RegexProjectSet r2) {
+				return r1.getRawPattern().compareTo(r2.getRawPattern());
+			}
+		}
+		TreeSet<RegexProjectSet> rawPatternOrderedCorpus = new TreeSet<RegexProjectSet>(new PatternComparator());
+		rawPatternOrderedCorpus.addAll(corpus);
+		HashMap<Integer, RegexProjectSet> theLookup = new HashMap<Integer, RegexProjectSet>();
+		int i = 0;
+		for (RegexProjectSet rps : rawPatternOrderedCorpus) {
+			theLookup.put(i++, rps);
+		}
+		return theLookup;
+	}
+
+	private static String getMapContent(TreeMap<String, Integer> anyMap) {
 		StringBuilder sb = new StringBuilder();
 		for (Entry<String, Integer> entry : anyMap.entrySet()) {
-			sb.append(entry.getValue()+"\t"+entry.getKey()+"\n");
+			sb.append(entry.getValue() + "\t" + entry.getKey() + "\n");
 		}
 		return sb.toString();
 	}
 
-	public static HashMap<String,Integer> getPatternIndexMap(String exportedCorpusRaw) throws IOException {
-		HashMap<String,Integer> patternIndexMap = new HashMap<String,Integer>();
+	public static HashMap<String, Integer> getPatternIndexMap(String exportedCorpusRaw) throws IOException {
+		HashMap<String, Integer> patternIndexMap = new HashMap<String, Integer>();
 		Pattern finder = Pattern.compile("(\\d+)\\t(\\d+)\\t(.*)");
 		Matcher pairMatcher = finder.matcher(exportedCorpusRaw);
 		while (pairMatcher.find()) {
@@ -74,7 +99,7 @@ public class MainScratch {
 		}
 		return patternIndexMap;
 	}
-	
+
 	private static HashMap<String, Integer> getPatternIndexMapFromCorpus(TreeSet<RegexProjectSet> corpus) {
 		/**
 		 * define a comparator that orders the regexes by rawPattern, as this
