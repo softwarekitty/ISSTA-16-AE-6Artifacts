13b121831f77f80f1d6f82618d7c5ab59d66cb09
Carl Chapman <carlallenchapman@gmail.com>
2016-05-01 12:08:03 -0500

    updating FeatureCountTest

--- src/test/core/features/FeatureCountTest.java
+++ src/test/core/features/FeatureCountTest.java
@@ -4,39 +4,49 @@ import static org.junit.Assert.assertFalse;
 
 import java.util.Arrays;
 
-import org.junit.BeforeClass;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import main.core.features.FeatureCount;
 import main.core.features.FeatureDictionary;
 
 public final class FeatureCountTest {
+	private static FeatureDictionary fd;
+	
+	@Before
+	public static void setup(){
+		fd = new FeatureDictionary();
+	}
 	
 	@Test
 	public void test_canCreateWithCorrectSizeArray() {
 		
 		// create feature count with all set to 1
-		FeatureDictionary fd = new FeatureDictionary();
 		int[] counts = new int[fd.getSize()];
-		FeatureCount fc = new FeatureCount(counts,fd);		
+		Arrays.fill(counts, 1);
+		FeatureCount fc = new FeatureCount(counts,fd);
+		Assert.assertNotNull(fc);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_cannotCreateWithSmallArray() {
 		
-		// create feature count with all set to 1
-		FeatureDictionary fd = new FeatureDictionary();
+		// create feature count with all set to 1, but array is too small
 		int[] counts = new int[fd.getSize()-1];
-		FeatureCount fc = new FeatureCount(counts,fd);		
+		Arrays.fill(counts, 1);
+		FeatureCount fc = new FeatureCount(counts,fd);	
+		Assert.assertNull(fc);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_cannotCreateWithLargeArray() {
 		
-		// create feature count with all set to 1
-		FeatureDictionary fd = new FeatureDictionary();
+		// create feature count with all set to 1, but array is too large
 		int[] counts = new int[fd.getSize()+1];
-		FeatureCount fc = new FeatureCount(counts,fd);		
+		Arrays.fill(counts, 1);
+		FeatureCount fc = new FeatureCount(counts,fd);	
+		Assert.assertNull(fc);
 	}
 	
 	@Test
@@ -45,16 +55,12 @@ public final class FeatureCountTest {
 		// create feature count with all set to 1
 		FeatureDictionary fd = new FeatureDictionary();
 		int[] counts = new int[fd.getSize()];
-		for(int i=0;i<counts.length;i++){
-			counts[i] = 1;
-		}
+		Arrays.fill(counts, 1);
 		FeatureCount fc = new FeatureCount(counts,fd);
 		
 		// now try to modify the array, setting all to 0
 		int[] shouldBeACopy = fc.getFeatureCountArray();
-		for(int i=0;i<counts.length;i++){
-			shouldBeACopy[i] = 0;
-		}
+		Arrays.fill(shouldBeACopy, 0);
 		assertFalse(Arrays.equals(shouldBeACopy, fc.getFeatureCountArray()));		
 	}
 }
